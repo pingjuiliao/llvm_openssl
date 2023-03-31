cb4c63bf5194448bcd46ecb11c755685ee159a02
Ping-Jui Liao <pingjuiliao@gmail.com>
2023-03-30 17:43:10 -0700

    trace file pass

--- llvm/llvm/lib/Transforms/Instrumentation/PrintFunc.cpp
+++ llvm/llvm/lib/Transforms/Instrumentation/PrintFunc.cpp
@@ -5,6 +5,7 @@ namespace llvm {
 PreservedAnalyses PrintFuncPass::run(Module& M, ModuleAnalysisManager& AM) {
   LLVMContext& C = M.getContext();
   IntegerType* Int32Ty = IntegerType::getInt32Ty(C);
+  IntegerType* Int64Ty = IntegerType::getInt64Ty(C);
   PointerType* Ptr8Ty = PointerType::getInt8PtrTy(C); 
   
 
@@ -16,10 +17,21 @@ PreservedAnalyses PrintFuncPass::run(Module& M, ModuleAnalysisManager& AM) {
     Instruction* FirstI = BB.getFirstNonPHI();
     IRBuilder<> IRB(FirstI);
     std::string s = "[LLVM TRACE] ";
-    s += F.getName().str();
+    s += F.getName().str() + "\n";
     Value* FuncName = IRB.CreateGlobalStringPtr(s);
-    FunctionCallee PutsFunc = M.getOrInsertFunction("puts", FunctionType::get(Int32Ty, {Ptr8Ty}));
-    IRB.CreateCall(PutsFunc, {FuncName});
+    FunctionCallee FopenFunc = M.getOrInsertFunction("fopen", FunctionType::get(Ptr8Ty, {Int32Ty, Ptr8Ty}, false));
+    Value* TraceFileName = IRB.CreateGlobalStringPtr("trace.txt");
+    Value* AppendMode = IRB.CreateGlobalStringPtr("a+");
+    Value* file_struct_ptr = IRB.CreateCall(FopenFunc, {TraceFileName, AppendMode});
+    
+    FunctionCallee FputsFunc = M.getOrInsertFunction("fputs", FunctionType::get(Int32Ty, {Ptr8Ty, Ptr8Ty}, false));
+    IRB.CreateCall(FputsFunc, {FuncName, file_struct_ptr});
+    
+    // FunctionType* WriteFuncTy = FunctionType::get(Int32Ty, {Int32Ty, Ptr8Ty, Int64Ty}, false);
+    // FunctionCallee WriteFunc = M.getOrInsertFunction("write", WriteFuncTy);
+    // IRB.CreateCall(WriteFunc, {IRB.getInt32(2), FuncName, IRB.getInt64(s.size())});
+
+    // IRB.CreateCall(PutsFunc, {FuncName});
   }
   return PreservedAnalyses::all();
 }
