d21aca782f1fbebf06b52f1b41f7d908d131918c
Ping-Jui Liao <pingjuiliao@gmail.com>
2023-03-31 14:59:37 -0700

    simplify shellscript

--- analysis/diff_test/enc_v_dec/diff.sh
+++ analysis/diff_test/enc_v_dec/diff.sh
@@ -2,18 +2,10 @@
 OPENSSL=../../../openssl/apps/openssl
 DIFF_PY=../../diff.py
 
-until ${OPENSSL} aes-256-cbc -a -e -pbkdf2 -in ./plaintext -pass "pass:example" -out ./ciphertext
-do
-  echo "Encrypting..."
-  sleep 1
-done
+${OPENSSL} aes-256-cbc -a -e -pbkdf2 -in ./plaintext -pass "pass:example" -out ./ciphertext
 mv trace.txt trace_enc.txt
 
-until ${OPENSSL} aes-256-cbc -a -d -pbkdf2 -in ./ciphertext -pass "pass:example" -out ./decrypted
-do
-  echo "Decrypting..."
-  sleep 1
-done
+${OPENSSL} aes-256-cbc -a -d -pbkdf2 -in ./ciphertext -pass "pass:example" -out ./decrypted
 mv trace.txt trace_dec.txt
 
 ${DIFF_PY} ./trace_enc.txt ./trace_dec.txt
