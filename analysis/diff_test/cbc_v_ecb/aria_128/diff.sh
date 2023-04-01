6d89471e74c9c2fcbd5f295ffc2196f67f3c6123
Ping-Jui Liao <pingjuiliao@gmail.com>
2023-03-31 17:50:26 -0700

    add differentail analysis: cbc vs ecb

---
+++ analysis/diff_test/cbc_v_ecb/aria_128/diff.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+OPENSSL=../../../../openssl/apps/openssl
+DIFF_PY=../../../diff.py
+
+${OPENSSL} aria-128-cbc -a -e -pbkdf2 -in ./plaintext -pass "pass:example" -out ciphertext_cbc
+mv trace.txt trace_cbc.txt
+
+${OPENSSL} aria-128-ecb -a -e -pbkdf2 -in ./plaintext -pass "pass:example" -out ciphertext_ecb
+mv trace.txt trace_ecb.txt
+
+${DIFF_PY} trace_cbc.txt trace_ecb.txt
